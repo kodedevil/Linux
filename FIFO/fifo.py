95466de1fe0eb73b53f9a7e8e71dd1b3c4416e25
Peer Springstübe <kratenko@garstig.org>
2018-10-21 14:52:23 +0200

    fix indents, funs, remove semis

--- FIFO/fifo.py
+++ FIFO/fifo.py
@@ -2,22 +2,23 @@ import os
 import errno
 import time
 
-bufferSize = 100;
-PATH = "pipe";
+bufferSize = 100
+PATH = "pipe"
 while 1:
-	try:
-		pipe = os.open(PATH, os.O_RDONLY | os.O_NONBLOCK);
-		input = os.read(pipe,bufferSize);
-	except OSError as err:
-		if err.errno == 11:
-			continue;
-		else:
-			raise err;
-	if input:
-		print(input);
+    try:
+        pipe = os.open(PATH, os.O_RDONLY | os.O_NONBLOCK)
+        input = os.read(pipe,bufferSize)
+    except OSError as err:
+        if err.errno == 11:
+            continue
+        else:
+            raise err
+    if input:
+        print(input)
 
-    os.close(pipe);
+    os.close(pipe)
 
     #Other functions
-    print "Sleep 500 ms"
-    time.sleep(0.5);	
+    print("Sleep 500 ms")
+    time.sleep(0.5)	
+
